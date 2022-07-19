1b65febf5e70937680fa937db9f152b2571c86b3
ErJump <fedelijunior@gmail.com>
2022-07-19 13:58:06 +0200

    feat: :sparkles: Adds getAlbums method to AlbumsList

--- src/components/AlbumsList.vue
+++ src/components/AlbumsList.vue
@@ -6,11 +6,32 @@
 
 <script>
 import AlbumCard from './AlbumCard.vue';
+import axios from 'axios';
 export default {
   name: 'AlbumsList',
   components: {
     AlbumCard,
-  }
+  },
+  data() {
+    return {
+      albums: [],
+    };
+  },
+  methods: {
+    getAlbums() {
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then(response => {
+          this.albums = response.data.response;
+          console.log(this.albums);
+        })
+        .catch(error => {
+          console.warn(error);
+        });
+    },
+  },
+  created() {
+    this.getAlbums();
+  },
 }
 </script>
 
