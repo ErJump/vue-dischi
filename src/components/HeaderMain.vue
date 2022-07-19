d993491a78eb25f082deaf3fbc4bd348da743ba7
ErJump <fedelijunior@gmail.com>
2022-07-19 13:33:49 +0200

    feat: :sparkles: Completes header main

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -1,19 +1,20 @@
 <template>
-  <div>
-    <h1>Header</h1>
+  <div class="my_header container-fluid py-2 px-4 d-flex justify-content-between">
+    <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png" alt="spotify logo">
   </div>
 </template>
 
 <script>
 export default {
   name: 'HeaderMain',
-  props: {
-    
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+@import "../assets/styles/variables.scss";
+  .my_header {
+    background-color: $headerBG;
+    height: 70px;
+  }
 </style>
