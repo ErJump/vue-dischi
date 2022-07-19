2cfd8ec3e5fceb8e564048cc5589e9b90816f275
ErJump <fedelijunior@gmail.com>
2022-07-19 13:39:58 +0200

    feat: :sparkles: Adds albumsList

---
+++ src/components/AlbumsList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>ciao io contengo gli albums</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlbumsList',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../assets/styles/variables.scss";
+  
+</style>
--- src/components/ContentMain.vue
+++ src/components/ContentMain.vue
@@ -1,19 +1,26 @@
 <template>
-  <div>
-    <h1>Main</h1>
+  <div class="my_main">
+    <div class="container-lg py-5">
+      <AlbumsList/>
+    </div>
   </div>
 </template>
 
 <script>
+import AlbumsList from './AlbumsList.vue';
 export default {
   name: 'ContentMain',
-  props: {
-    
+  components: {
+    AlbumsList,
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+@import "../assets/styles/variables.scss";
+  .my_main {
+    background-color: $mainBG;
+    height: calc(100vh - 70px);
+  }
 </style>
